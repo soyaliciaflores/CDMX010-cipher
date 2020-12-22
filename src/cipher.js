e235054b70cdb07b91d4bcacd7f65abda1e7aabc
Alicia Flores <floresalvarado.alicia@gmail.com>
2020-12-22 12:25:00 -0600

    Primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,25 @@
 const cipher = {
-  // ...
-};
-
-export default cipher;
+  //AQUI EMPIEZA LA MAGIA
+    encode:function (texto, num2){
+     let alphabet= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+     let mayus = texto.toUpperCase();
+     let result=[]
+     let espacio= " "
+      
+      for (let i=0;i<mayus.length;i++){ 
+      let indiceInicial= mayus.charAt(i);
+  
+  if (indiceInicial===" "){
+      result.push(espacio)
+  }
+  else{ 
+      result.push(alphabet[(alphabet.indexOf(indiceInicial)+num2)%26])
+        } 
+      }
+    return result.join("")
+    }
+    }
+  
+    
+  export default cipher; 
+  
--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+// Test case
+//console.log(cipher.encode("HOLA CHAVAS", 1));
+
+let botonEnviar = document.getElementById("boton1");
+botonEnviar.addEventListener("click", function() {
+    const num = document.getElementById("posiciones").value;
+    const num2 = parseInt(num);
+    const texto = document.getElementById("nombre").value;
+    document.getElementById("formularioCifrar").style.display = "none";
+    document.getElementById("nombre2").innerHTML = cipher.encode(texto, num2);
+   
+    document.getElementById("puesto2").innerHTML=document.getElementById("puesto").value;
+    document.getElementById("sueldo2").innerHTML=document.getElementById("sueldo").value;
+    document.getElementById("escolaridad2").innerHTML=document.getElementById("escolaridad").value;
+    document.getElementById("idiomas2").innerHTML=document.getElementById("idiomas").value;
+    document.getElementById("habilidades2").innerHTML=document.getElementById("habilidades").value;
+    document.getElementById("experiencia2").innerHTML=document.getElementById("experiencia").value;
+    document.getElementById("comentarios2").innerHTML=document.getElementById("comentarios").value;
+});
+
+import decipher from './decipher.js';
+// Test case
+//console.log(decipher.decode("BMJDJB IPMB", 1));
+let botonEnviar2 = document.getElementById("boton2");
+botonEnviar2.addEventListener("click", function() {
+    const num3 = document.getElementById("posiciones2").value;
+    const num4 = parseInt(num3);
+    const texto1 = document.getElementById("nombre5").value;
+    document.getElementById("nombre4").innerHTML = decipher.decode(texto1, num4);
+    document.getElementById("celular2").innerHTML=document.getElementById("celular").value;
+    document.getElementById("email2").innerHTML=document.getElementById("email").value;
+});
+
